d31c3584017ef727d6209fb2da082498fe28cc1f
David Robert <nyamwangedavid2019@gmail.com>
2024-08-24 22:43:40 +0300

    handled email verification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/firebase_options.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,6 +10,39 @@ void main() {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('user is verified');
+              } else
+                ;
+              print('You need to verify you email.');
+              print(user);
+              return const Text('Done');
+            default:
+              return const Text('Loading.....');
+          }
+        },
+      ),
+    );
+  }
 }
